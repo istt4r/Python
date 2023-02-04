9d15ed2386173fe76fbd730ca24b2c2af96306c4
istt4r <84396916+istt4r@users.noreply.github.com>
2023-02-04 19:25:18 +0000

    converting month name to numerical format

--- copy & rename file.py
+++ copy & rename file.py
@@ -19,18 +19,38 @@ def copy_and_rename_file(root_dir, dest_dir):
                         
                         date_parts = date.split()
                         month = date_parts[0]
+                        
+                        # Create a dictionary to map month names to their numerical values
+                        month_map = {
+                            "January": "01",
+                            "February": "02",
+                            "March": "03",
+                            "April": "04",
+                            "May": "05",
+                            "June": "06",
+                            "July": "07",
+                            "August": "08",
+                            "September": "09",
+                            "October": "10",
+                            "November": "11",
+                            "December": "12"
+                        }
+
+                        # Use the get method to get the numerical value for the month
+                        month_num = month_map.get(month, "")
                         day = date_parts[1].strip(',')
                         year = date_parts[2]
-                        format_date = f"{year}_{month}_{day}"
+                        format_date = f"{year}_{month_num}_{day}"
                         print(f"Format_Date: {format_date}")  # Debugging statement
                                             
-                        session_match = re.search(r'(back|biceps|chest|deltoid|forearm|legs 1|legs 2|push 1|push 2|pull 1|pull 2|triceps|abdominals)', path.name, re.IGNORECASE)
+                        session_match = re.search(r'(back|biceps|chest|deltoid|forearm|triceps|abdominals|legs one|legs two|push one|push two|pull one|pull two)', path.name, re.IGNORECASE)
                         print(f"Session_Match: {session_match}")  # Debugging statement
                         
                         if session_match:
                             session = session_match.group(0)
-                            new_filename = format_date + "_" + session + ".csv"
+                            new_filename = format_date + ": " + session + ".csv"
                             notion_export_id = path.name.split('_')[-1]
+                            
                             dest_path = Path(dest_dir)
                             dest_file_path = dest_path / new_filename
                             if not dest_file_path.exists():
