bc7faf7ec934951a060d5c59355619c76a571655
istt4r <84396916+istt4r@users.noreply.github.com>
2023-02-04 23:01:13 +0000

    restarting notion export

--- Notion_Daily_Export.py
+++ Notion_Daily_Export.py
@@ -2,6 +2,7 @@ import csv
 import datetime
 import notion_client
 import config
+from config import ROOT_DIR
 
 # Initialize a Notion client
 notion = notion_client.Client(auth={"token_v2": config.NOTION_API_TOKEN})
@@ -9,12 +10,3 @@ notion = notion_client.Client(auth={"token_v2": config.NOTION_API_TOKEN})
 # Get the current date
 today = datetime.datetime.now().strftime("%Y-%m-%d")
 
-# Search for database entries with a "Date" field equal to today
-results = notion.search(filter={"property": "Date", "value": today}).get("results")
-
-# Write the results to a CSV file
-with open("entries.csv", "w", newline="") as f:
-    writer = csv.writer(f)
-    writer.writerow(["Title", "Date", "Description"])
-    for result in results:
-        writer.writerow([result["title"], result["Date"], result["Description"]])
