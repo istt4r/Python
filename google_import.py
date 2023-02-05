a0fb74f50176190ae04a2f0be52ee5c94db8507a
istt4r <84396916+istt4r@users.noreply.github.com>
2023-02-05 21:41:17 +0000

    cleanup, -- debugging, ++ comments

--- google_import.py
+++ google_import.py
@@ -1,7 +1,7 @@
 import gspread
 from pathlib import Path
 from google.oauth2.service_account import Credentials
-from config import CREDENTIALS, CSV_TEST, ROOT_DIR
+from config import CREDENTIALS, DEST_DIR
 
 scope = [
     "https://spreadsheets.google.com/feeds",
@@ -20,54 +20,45 @@ client = gspread.authorize(credentials)
 spreadsheet = client.open("Notion Fitness CSV Import")
 
 starting_cell = "A1"
-content_file = r"C:/Users/jecla/OneDrive/Desktop/FitExport/Content.txt"
-staging_file = r"C:/Users/jecla/OneDrive/Desktop/FitExport/Staging.txt"
 
-def import_csv(root_dir, sheet, cell):
-    '''
-    path - root directory of .csv files to be uploaded
-    sheet - a gspread.Spreadsheet object
-    cell - string giving starting cell, optionally including sheet/tab name
-      ex: 'A1', 'MySheet!C3', etc.
-    '''    
+
+# root_dir - root directory of .csv files to be uploaded
+# spreadsheet - a gspread.Spreadsheet object 
+# cell - desired cell (row/column) to insert data when uploading to spreadsheet
+#  ex: 'A1', 'MySheet!C3', etc.
+
+def import_csv(root_dir, sheet, cell):   
     if '!' in cell:
         (tabName, cell) = cell.split('!')
         wks = sheet.worksheet(tabName)
     else:
         wks = sheet.sheet1
     (firstRow, firstColumn) = gspread.utils.a1_to_rowcol(cell)
-    print(f"firstRow, firstColumn: {firstRow}, {firstColumn}")
     
     root = Path(root_dir)
 
+    # reading all .csv files, and appending the successive text to content_data
     content_data = ""
     for path in root.rglob('*.csv'):
         with open(path, 'r', encoding='utf-8') as f:
             print(f"Path: {path}")
             content_data += f.read() + "\n"
-
-    # writing to the content_file
-    with open(content_file, "w", encoding='utf-8') as content:
-        content.write(content_data)
-            
-    with open(staging_file, "w", encoding='utf-8') as staging:
-        body = {
-            'requests': [{
-                'pasteData': {
-                    "coordinate": {
-                        "sheetId": wks.id,
-                        "rowIndex": firstRow-1,
-                        "columnIndex": firstColumn-1,
-                    },
-                    "data": content_data,
-                    "type": 'PASTE_NORMAL',
-                    "delimiter": ',',
-                }
-            }]
-        }
-        staging.write(f"{body}\n")
-        print(f"Body: {body}")
-        sheet.batch_update(body)
+        
+    body = {
+        'requests': [{
+            'pasteData': {
+                "coordinate": {
+                    "sheetId": wks.id,
+                    "rowIndex": firstRow-1,
+                    "columnIndex": firstColumn-1,
+                },
+                "data": content_data,
+                "type": 'PASTE_NORMAL',
+                "delimiter": ',',
+            }
+        }]
+    }
+    sheet.batch_update(body)
     
 
-import_csv(ROOT_DIR, spreadsheet, starting_cell)
+import_csv(DEST_DIR, spreadsheet, starting_cell)
