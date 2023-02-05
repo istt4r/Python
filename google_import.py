bfb864da4d7b3576283e773ed0ff2f756d77a748
istt4r <84396916+istt4r@users.noreply.github.com>
2023-02-05 21:12:04 +0000

    working google_import

--- google_import.py
+++ google_import.py
@@ -1,6 +1,7 @@
 import gspread
+from pathlib import Path
 from google.oauth2.service_account import Credentials
-from config import CREDENTIALS, CSV_TEST
+from config import CREDENTIALS, CSV_TEST, ROOT_DIR
 
 scope = [
     "https://spreadsheets.google.com/feeds",
@@ -18,12 +19,13 @@ client = gspread.authorize(credentials)
 
 spreadsheet = client.open("Notion Fitness CSV Import")
 
-cell_start = "A3"
+starting_cell = "A1"
+content_file = r"C:/Users/jecla/OneDrive/Desktop/FitExport/Content.txt"
+staging_file = r"C:/Users/jecla/OneDrive/Desktop/FitExport/Staging.txt"
 
-
-def import_csv(csv, sheet, cell):
+def import_csv(root_dir, sheet, cell):
     '''
-    csv - path to csv file to upload
+    path - root directory of .csv files to be uploaded
     sheet - a gspread.Spreadsheet object
     cell - string giving starting cell, optionally including sheet/tab name
       ex: 'A1', 'MySheet!C3', etc.
@@ -35,23 +37,37 @@ def import_csv(csv, sheet, cell):
         wks = sheet.sheet1
     (firstRow, firstColumn) = gspread.utils.a1_to_rowcol(cell)
     print(f"firstRow, firstColumn: {firstRow}, {firstColumn}")
+    
+    root = Path(root_dir)
 
-    with open(csv, 'r', encoding='utf-8') as f:
-        csvContents = f.read()
-    body = {
-        'requests': [{
-            'pasteData': {
-                "coordinate": {
-                    "sheetId": wks.id,
-                    "rowIndex": firstRow-1,
-                    "columnIndex": firstColumn-1,
-                },
-                "data": csvContents,
-                "type": 'PASTE_NORMAL',
-                "delimiter": ',',
-            }
-        }]
-    }
-    return sheet.batch_update(body)
+    content_data = ""
+    for path in root.rglob('*.csv'):
+        with open(path, 'r', encoding='utf-8') as f:
+            print(f"Path: {path}")
+            content_data += f.read() + "\n"
 
-import_csv(CSV_TEST, spreadsheet, cell_start)
+    # writing to the content_file
+    with open(content_file, "w", encoding='utf-8') as content:
+        content.write(content_data)
+            
+    with open(staging_file, "w", encoding='utf-8') as staging:
+        body = {
+            'requests': [{
+                'pasteData': {
+                    "coordinate": {
+                        "sheetId": wks.id,
+                        "rowIndex": firstRow-1,
+                        "columnIndex": firstColumn-1,
+                    },
+                    "data": content_data,
+                    "type": 'PASTE_NORMAL',
+                    "delimiter": ',',
+                }
+            }]
+        }
+        staging.write(f"{body}\n")
+        print(f"Body: {body}")
+        sheet.batch_update(body)
+    
+
+import_csv(ROOT_DIR, spreadsheet, starting_cell)
