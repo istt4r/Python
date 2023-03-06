10f594a6c77a88d98592f0a99a819323a405c238
istt4r <84396916+istt4r@users.noreply.github.com>
2023-03-06 22:58:52 +0000

    refactor

--- standards_exercise_table.py
+++ standards_exercise_table.py
@@ -1,16 +1,17 @@
 import csv
-from pathlib import Path
 import datetime
-from config import SS_DATA_PATH
+from pathlib import Path
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+from config import SS_DATA_PATH
 
 
-def get_table_data(driver, exercise, category, gender, table_xpath, today):
+def get_table_data(driver, exercise_name, category, gender, table_xpath, today):
     if category not in ["age", "weight"]:
         raise ValueError("Invalid category specified")
 
@@ -24,7 +25,7 @@ def get_table_data(driver, exercise, category, gender, table_xpath, today):
             continue
         columns = row.find_elements(By.TAG_NAME, "td")
         category_data = {
-            'exercise': exercise,
+            'exercise': exercise_name,
             'datecollected': today,
             'category': category,
             'gender': gender,
@@ -38,55 +39,52 @@ def get_table_data(driver, exercise, category, gender, table_xpath, today):
         table_data.append(category_data)
     return table_data
 
+
 def scrape_exercise_data(driver, exercise_name, exercise_records, url, image_url):
     driver.switch_to.new_window('tab')
     driver.get(url)
 
-    # buttons to select to display table data by weight, age, and gender
-    male_button = driver.find_element(By.XPATH, "//li[@data-tab='Male']//a")
-    female_button = driver.find_element(By.XPATH, "//li[@data-tab='Female']//a")
-
-    # xpath definitions
-    scroll_position_xpath = "//div[@data-tab-group='Standards Exercise']"
-    table_xpath = "//table[(ancestor::div[contains(@data-tab-group, 'By Weight and Age')])]//tr"
-    
-     # Define the categories and genders to fetch data for
+    # Define the categories and genders to fetch data for
     categories = ["weight", "age"]
     genders = ["male", "female"]
 
     today = datetime.date.today()
 
-    # position the driver view optimally
-    def scroll_position_exercise(xpath):
-        element = driver.find_element(By.XPATH, xpath)
-        y_coord = element.location['y']
-        driver.execute_script("window.scrollTo(0, {})".format(y_coord))
-    scroll_position_exercise(scroll_position_xpath)
-    
+    # Define the xpath for scrolling to the exercise element
+    scroll_position_xpath = "//div[@data-tab-group='Standards Exercise']"
+
+    # Scroll to the exercise element
+    element = driver.find_element(By.XPATH, scroll_position_xpath)
+    y_coord = element.location['y']
+    driver.execute_script("window.scrollTo(0, {})".format(y_coord))
+
     table_data = []
     for gender in genders:
         for category in categories:
-            if gender == "male":
-                male_button.click()
-            elif gender == "female":
-                female_button.click()
+            # Get the button for the selected gender
+            gender_button = driver.find_element(By.XPATH, f"//li[@data-tab='{gender.capitalize()}']//a")
+            gender_button.click()
 
             if category == "weight":
+                # Get the button for selecting data by bodyweight
                 weight_button = driver.find_element(By.XPATH, "//a[text()='By Bodyweight' and not(ancestor::div[contains(@class, 'is-hidden')])]")
                 weight_button.click()
-                table_xpath = table_xpath if gender == "male" else table_xpath
             elif category == "age":
+                # Get the button for selecting data by age
                 age_button = driver.find_element(By.XPATH, "//a[text()='By Age' and not(ancestor::div[contains(@class, 'is-hidden')])]")
                 age_button.click()
-                table_xpath = table_xpath if gender == "male" else table_xpath
 
             # Get the table data
+            table_xpath = "//table[(ancestor::div[contains(@data-tab-group, 'By Weight and Age')])]//tr"
+            if gender == "female":
+                table_xpath += "[not(contains(@class, 'male'))]"
             data = get_table_data(driver, exercise_name, category, gender, table_xpath, today)
             table_data.extend(data)
 
     # Merge all the data into one object
     merged_data = {'data': table_data}
-            
+
+    # Write the data to a CSV file
     merge_path = Path(SS_DATA_PATH) / f"{exercise_name}.csv"
     with merge_path.open(mode='w', newline='') as csvfile:
         if merged_data['data']:
--- standards_exercises.py
+++ standards_exercises.py
@@ -2,31 +2,20 @@ import time
 import random
 from pathlib import Path
 import csv
+from urllib.parse import urljoin
+
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
 from config import URL, SS_INFO_PATH
-from bs4 import BeautifulSoup
 import standards_exercise_table
-from urllib.parse import urljoin
 
-# set the path and filename for the output file
-info_file = Path(SS_INFO_PATH)
-# create a new Firefox browser window
-driver = webdriver.Firefox()
-driver.maximize_window()
-driver.get(URL)
 
-# Get the handle of the original tab or window
-original_window_handle = driver.current_window_handle
-
-more_exercises_xpath = "//button[normalize-space()='More Exercises...']"
-more_exercises_button = driver.execute_script('return document.querySelector(".py-2 > button:nth-child(1)")')
-
-# click "agree" button in GDPR popup window
-def accept_gdpr():
-    # wait for the GDPR agreement policy to load and click the button to agree to the terms
+def accept_gdpr(driver):
+    """Clicks the "Agree" button in the GDPR popup window."""
     try:
         gdpr_agree_button = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, 'button.sc-ifAKCX:nth-child(2)'))
@@ -34,60 +23,76 @@ def accept_gdpr():
         gdpr_agree_button.click()
     except:
         print('GDPR agreement button not found or not clickable\n')
-accept_gdpr()
 
-# position the driver view optimally
-def scroll_position(xpath):
+
+def scroll_position(driver, xpath):
+    """Positions the driver view optimally."""
     element = driver.find_element(By.XPATH, xpath)
     y_coord = element.location['y'] - 300
     driver.execute_script("window.scrollTo(0, {})".format(y_coord))
 
 
-# click "more exercises" button until all exercises are displayed on page
-def expand_exercises():
+def expand_exercises(driver):
+    """Clicks the "More Exercises" button until all exercises are displayed on page."""
+    more_exercises_xpath = "//button[normalize-space()='More Exercises...']"
+    more_exercises_button = driver.execute_script('return document.querySelector(".py-2 > button:nth-child(1)")')
+
     while True:
         try:
             time.sleep(random.uniform(0.5, 1))
             driver.execute_script("arguments[0].click();", more_exercises_button)
-            scroll_position(more_exercises_xpath)
-            if more_exercises_button.is_enabled() == False:
+            scroll_position(driver, more_exercises_xpath)
+            if not more_exercises_button.is_enabled():
                 print("Button is disabled")
                 break
         except:
             print("More exercises button not found or not clickable")
             pass
-expand_exercises()
 
-# extract the HTML content after all the exercises have been loaded
-html_content = driver.page_source
 
-# create a BeautifulSoup object
-soup = BeautifulSoup(html_content, 'html.parser')
-
-# find all div elements with class="exerciseitem"
-exercise_items = soup.find_all('div', {'class': 'exerciseitem'})
-
-exercise_standards = []
-def get_exercise_item_data():
-    #url = item.find('a')['href']
+def get_exercise_item_data(driver, item):
+    """Extracts the exercise data from an exercise item."""
     url = urljoin("https://strengthlevel.com", item.find('a')['href'])
     img_url = item.find('img')['data-src']
     exercise_name = item.find('span').text.strip()
     exercise_records = item.find_all('span')[1].text.strip()
-    
-    # writer.writerow([exercise_name, exercise_records, url, img_url])
 
-    # print the extracted information
     print(f'Exercise Name: {exercise_name}')
     print(f'Exercise Records: {exercise_records}')
     print(f'URL: {url}')
     print(f'Image URL: {img_url}\n')
-    
-    standards_exercise_table.scrape_exercise_data(driver, exercise_name,exercise_records,url,img_url)
-    driver.switch_to.window(original_window_handle)
-    
-for item in exercise_items:
-    get_exercise_item_data()
 
-driver.quit()
+    standards_exercise_table.scrape_exercise_data(driver, exercise_name, exercise_records, url, img_url)
+    driver.switch_to.window(driver.window_handles[0])
 
+
+# Set the path and filename for the output file
+info_file = Path(SS_INFO_PATH)
+
+# Create a new Firefox browser window
+with webdriver.Firefox() as driver:
+    driver.maximize_window()
+    driver.get(URL)
+
+    # Get the handle of the original tab or window
+    original_window_handle = driver.current_window_handle
+
+    # Click "Agree" button in GDPR popup window
+    accept_gdpr(driver)
+
+    # Click "More Exercises" button until all exercises are displayed on page
+    expand_exercises(driver)
+
+    # Extract the HTML content after all the exercises have been loaded
+    html_content = driver.page_source
+
+    # Create a BeautifulSoup object
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    # Find all div elements with class="exerciseitem"
+    exercise_items = soup.find_all('div', {'class': 'exerciseitem'})
+
+    for item in exercise_items:
+        get_exercise_item_data(driver, item)
+
+    driver.quit()
